6f9cd69b9743a367cd071d6f332e39ef0784d048
rshan <zhangys3@infore.com>
2024-06-17 17:13:08 +0800

    add: sync status record

--- .github/tools/skopeo-copy.sh
+++ .github/tools/skopeo-copy.sh
@@ -10,11 +10,40 @@ SKOPEO_USERNAME=${USERNAME}
 SKOPEO_PASSWORD=${PASSWORD}
 SKOPEO_AUTHFILE=${AUTHFILE:-'./containers/auth.json'}
 
+getSyncStatus()
+## 获取同步状态，参数：注册表名称, 仓库名称， Tag名称
+{
+    local REGISTRY=$1
+    local REPOSITORY=$2
+    local TAG=$3
+    local SYNCSTATUS_FILE=${4:-$IMAGES_FILE}.sync
+
+    if [ ! -f "${SYNCSTATUS_FILE}" ]; then
+        echo -e "# 已同步镜像列表\n" >${SYNCSTATUS_FILE}
+    fi
+    eval $(echo "yq -e '.\"${REGISTRY}\".images.\"${REPOSITORY}\".[] | select (. == \"${TAG}\")' ${SYNCSTATUS_FILE}") 2>/dev/null
+    return $?
+}
+
+updateSyncStatus()
+## 写入同步结果，参数：注册表名称, 仓库名称， Tag名称
+{
+    local REGISTRY=$1
+    local REPOSITORY=$2
+    local TAG=$3
+    local SYNCSTATUS_FILE=${4:-$IMAGES_FILE}.sync
+
+    local line=$(eval $(echo "yq '.\"${REGISTRY}\".images.\"${REPOSITORY}\".[]' ${SYNCSTATUS_FILE} | wc -l"))
+    eval $(echo "yq -i '.\"${REGISTRY}\".images.\"${REPOSITORY}\".["${line}"] = \"${TAG}\"' ${SYNCSTATUS_FILE}")
+    eval $(echo "yq -i '.\"${REGISTRY}\".updated = now' ${SYNCSTATUS_FILE}")
+}
+
 getRegistry()
 ## 获取注册表地址列表
 {
     local IMAGES_FILE=${1:-$IMAGES_FILE}
-    local registry=$(yq -e 'keys' ${IMAGES_FILE} | awk '{print $2}')
+
+    local registry=$(yq 'keys' ${IMAGES_FILE} | awk '{print $2}')
     echo $registry
 }
 
@@ -23,7 +52,8 @@ getRepository()
 {
     local REGISTRY=$1
     local IMAGES_FILE=${2:-$IMAGES_FILE}
-    local repository=$(eval $(echo "yq -e '.\"${REGISTRY}\".images[] | key' ${IMAGES_FILE}"))
+
+    local repository=$(eval $(echo "yq '.\"${REGISTRY}\".images[] | key' ${IMAGES_FILE}"))
     echo $repository
 }
 
@@ -33,7 +63,8 @@ getTag()
     local REGISTRY=$1
     local REPOSITORY=$2
     local IMAGES_FILE=${3:-$IMAGES_FILE}
-    local tags=$(eval $(echo "yq -e '.\"${REGISTRY}\".images.\"${REPOSITORY}\"' ${IMAGES_FILE} ") | awk '{print $2}')
+
+    local tags=$(eval $(echo "yq '.\"${REGISTRY}\".images.\"${REPOSITORY}\"' ${IMAGES_FILE} ") | awk '{print $2}')
     echo $tags
 }
 
@@ -54,8 +85,11 @@ do
         # TAGS=$(getTag $reg $repo)
         for tag in $(getTag $reg $repo); 
         do
-            echo "+ skopeo copy docker://${reg}/${repo}:${tag} docker://${IMAGES_REPO}/${repo}:${tag}"
-            skopeo copy --retry-times 5 docker://${reg}/${repo}:${tag} docker://${IMAGES_REPO}/${repo}:${tag}
+            if [ ! $(getSyncStatus ${reg} ${repo} ${tag}) ]; then
+                echo "+ skopeo copy docker://${reg}/${repo}:${tag} docker://${IMAGES_REPO}/${repo}:${tag}"
+                skopeo copy --retry-times 5 docker://${reg}/${repo}:${tag} docker://${IMAGES_REPO}/${repo}:${tag}
+                updateSyncStatus ${reg} ${repo} ${tag}
+            fi
         done
     done
 done
